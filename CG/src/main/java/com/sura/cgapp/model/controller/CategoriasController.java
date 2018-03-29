37c6f9a993364c50307e45bc4c3eaf42bd872ccf
luis <luis@luis-PC>
2018-03-28 23:09:03 -0500

    segundo commit

--- CG/src/main/java/com/sura/cgapp/model/controller/CategoriasController.java
+++ CG/src/main/java/com/sura/cgapp/model/controller/CategoriasController.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.sura.cgapp.model.dao.CategoryDaoInterface;
+import com.sura.cgapp.model.entity.CategoryEntity;
 
 @Controller
 public class CategoriasController {
@@ -17,5 +19,17 @@ public class CategoriasController {
 	public String listarCategorias(Model model) {		
 		model.addAttribute("listCategories", categoryDao.categoryFindAll());
 		return "categorias";
+	}
+	
+	@GetMapping("/newCategory")
+	public String newCategory(Model model) {
+		model.addAttribute("category",new CategoryEntity());
+		return "nuevaCategoria";
+	}
+	
+	@PostMapping("/newCategory")
+	public String createCategory(Model model) {
+		model.addAttribute("category",new CategoryEntity());
+		return "nuevaCategoria";
 	}
 }
