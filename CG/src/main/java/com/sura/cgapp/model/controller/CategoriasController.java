ec6946fa64095697be3cc0554c5385c27eadddd8
luis <luis@luis-PC>
2018-04-01 23:32:14 -0500

    capa de servicio

--- CG/src/main/java/com/sura/cgapp/model/controller/CategoriasController.java
+++ CG/src/main/java/com/sura/cgapp/model/controller/CategoriasController.java
@@ -7,18 +7,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import com.sura.cgapp.model.dao.CategoryDaoInterface;
 import com.sura.cgapp.model.entity.CategoryEntity;
+import com.sura.cgapp.model.services.CategoryServices;
 
 @Controller
 public class CategoriasController {
 	
 	@Autowired
-	private CategoryDaoInterface categoryDao;
+	private CategoryServices categoryServices;
 	
 	@GetMapping("/categorias")
 	public String listarCategorias(Model model) {		
-		model.addAttribute("listCategories", categoryDao.findAll());
+		model.addAttribute("listCategories", categoryServices.findAll());
 		return "categorias";
 	}
 	
@@ -30,14 +30,13 @@ public class CategoriasController {
 	
 	@PostMapping("/newCategory")
 	public String createCategory(CategoryEntity categoryEntity) {		
-		categoryDao.save(categoryEntity);
+		categoryServices.save(categoryEntity);
 		return "redirect:categorias";
 	}
 	
 	@GetMapping(value="/delete/{id}")
 	public String eliminarCategoria(@PathVariable(value="id") Integer id) {
-		CategoryEntity c = categoryDao.findOne(id);		
-		categoryDao.delete(c);
+		categoryServices.delete(id);
 		return "redirect:/categorias";
 	}
 }
---
+++ CG/src/main/java/com/sura/cgapp/model/services/CategoryServices.java
@@ -0,0 +1,35 @@
+package com.sura.cgapp.model.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sura.cgapp.model.dao.CategoryDaoInterface;
+import com.sura.cgapp.model.entity.CategoryEntity;
+
+@Service
+public class CategoryServices {
+	
+	@Autowired
+	private CategoryDaoInterface categoryDao;
+	
+	public List<CategoryEntity> findAll(){
+		return (List<CategoryEntity>)categoryDao.findAll();
+	}
+	
+	public CategoryEntity findOne(Integer id) {
+		return categoryDao.findOne(id);		
+	}
+	
+	public void save(CategoryEntity categoryEntity) {
+		categoryDao.save(categoryEntity);
+	}
+	
+	public void delete(Integer id) {
+		categoryDao.delete(findOne(id));
+	}
+	
+	
+
+}
