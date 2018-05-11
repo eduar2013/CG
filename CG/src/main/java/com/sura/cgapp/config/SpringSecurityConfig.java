bf0269bb099f7d4cf16a7fcdeec1a405f2654693
luissata <luissata@PEPS3D32K72.suramericana.com.co>
2018-05-11 15:44:41 -0500

    login

--- CG/src/main/java/com/sura/cgapp/config/SpringSecurityConfig.java
+++ CG/src/main/java/com/sura/cgapp/config/SpringSecurityConfig.java
@@ -6,19 +6,25 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import com.sura.cgapp.auth.handler.LoginSuccesHandler;
+
 @Configuration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
 	
-	
+	@Autowired
+	private LoginSuccesHandler successHandler;
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/","/css/**","/js/**","/images/**").permitAll()
+		.antMatchers("/categorias/**").hasAnyRole("ADMIN")
 		.antMatchers("/subcategories/**").hasAnyRole("ADMIN")
 		.antMatchers("/egreso/**").hasAnyRole("ADMIN","USER")
+		.antMatchers("/").hasAnyRole("ADMIN","USER")
 		.anyRequest().authenticated()
 		.and()
-		.formLogin().loginPage("/login").permitAll()
+		.formLogin().successHandler(successHandler)
+		.loginPage("/login").permitAll()
 		.and()
 		.logout().permitAll()
 		.and()
@@ -30,5 +36,4 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
 		build.inMemoryAuthentication().withUser("admin").password("admin").roles("ADMIN","USER");
 		build.inMemoryAuthentication().withUser("andres").password("123").roles("USER");
 	}
-
 }
--- CG/src/main/java/com/sura/cgapp/model/controller/EgresoController.java
+++ CG/src/main/java/com/sura/cgapp/model/controller/EgresoController.java
@@ -1,7 +1,13 @@
 package com.sura.cgapp.model.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.web.servletapi.SecurityContextHolderAwareRequestWrapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,12 +29,30 @@ public class EgresoController {
 	@Autowired
 	SubCategoryServices subcategoryServices;
 	
+	
+	Log logger = LogFactory.getLog(EgresoController.class);
+	
 	@GetMapping({"/","/listarEgresos"})
-	public String listarEgresos(Model model) {
+	public String listarEgresos(Model model,HttpServletRequest request) {
 		model.addAttribute("egreso", new Egreso());
 		model.addAttribute("egresosList",egresoServices.findAll());
 		model.addAttribute("title", "Gastos");
 		model.addAttribute("listSubCategorias", subcategoryServices.getSubCategories());
+		
+		
+		SecurityContextHolderAwareRequestWrapper securityContext = new SecurityContextHolderAwareRequestWrapper(request,"ROLE_");
+		if(securityContext.isUserInRole("ADMIN")) {
+			logger.info("Usuario tiene el Rol 'ROLE_ADMIN'");
+		}else {
+			logger.info("Usuario NOO tiene el Rol 'ROLE_ADMIN'");
+		}
+		
+		
+		if(request.isUserInRole("ROLE_ADMIN")) {
+			logger.info("Usuario tiene el Rol 'ROLE_ADMIN' validando con request");
+		}else {
+			logger.info("Usuario NOOO tiene el Rol 'ROLE_ADMIN' validando con request");
+		}
 		return "egreso/egresoView";
 	}
 	
